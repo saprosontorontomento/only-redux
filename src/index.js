99eba6718a457459899703d1b98e5e8ffa30b968
econom1st <camcycamovu4@gmail.com>
2021-06-20 09:40:24 +0300

    destuct bindActionCreators

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
-import { createStore } from "redux";
+import { createStore, bindActionCreators } from "redux";
 import reducer from './reducer';
 import {inc, dec, res} from './action';
 
 const store = createStore(reducer);
 const {dispatch} = store;
 
-const bindActionCreator = (creator, dispatch) => (...args) => { // для наглядности как работает встроенная func в redux
-    dispatch(creator(...args));
-}
+const {incDispatch, decDispatch, resDispatch} = bindActionCreators (
+    {
+        incDispatch: inc,
+        decDispatch: dec,
+        resDispatch: res
+    }
+    , dispatch);
 
-const incDispatch = bindActionCreator(inc, dispatch);
-const decDispatch = () => dispatch(dec());
-const resDispatch = () => dispatch(res());
+
+// const decDispatch = bindActionCreators(dec, dispatch);
+// const resDispatch = bindActionCreators(res, dispatch);
 
 document.getElementById('inc').addEventListener('click', incDispatch);
 document.getElementById('dec').addEventListener('click', decDispatch);
